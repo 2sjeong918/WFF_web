cd93e449037b142067ade3276d5aa9a3bbd85cb6
aeree <dofl5576@gmail.com>
2017-11-28 21:26:23 +0900

    combine

--- js/scroll.js
+++ js/scroll.js
@@ -1,70 +1,75 @@
 (function(global, $, SM){
   'use strict';
-  // scroll magic
+  $( window ).scroll(function() {
+    // $output.html( scrolling );
+    console.log('scrolling');
+    $('.about-content-bg').addClass('about-content-bg-act');
+    $('.wow-content-bg').css( {"backgroundImage" :"url('../imgs/wow-bg-act.svg')"});
+    $('.films-content-bg').addClass('films-content-bg-act');
+    // $('.usb-content-bg').addClass('usb-content-bg-act');
+    
+    clearTimeout( $.data( this, "scrollCheck" ) );
+    $.data( this, "scrollCheck", setTimeout(function() {
+      $('.about-content-bg').removeClass('about-content-bg-act');
+      $('.wow-content-bg').css( {"backgroundImage" :"linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('../imgs/wow-bg.svg')"});
+      $('.films-content-bg').removeClass('films-content-bg-act');
+      // $('.usb-content-bg').removeClass('usb-content-bg-act');
+    }, 500) );
+  });
 
-  // Controller 설정
+  $('.order-detail button').on('mouseenter mouseleave', function( event ) {
+    $('.usb-content-bg').toggleClass( "usb-content-bg-act" );
+  })
+
+  
   var ctrl = new SM.Controller();
 
-  // 핀설정
-  var intro_pin = new SM.Scene({
-    'triggerElement': '#intro',
-    'triggerHook' : 0,
-    'duration' : 600
-  });
-  intro_pin
-    .setPin('#intro', {'pushFollowers': false})
-    .addTo(ctrl)
-    .addIndicators({
-      'name': 'intro pin'
-    })
-    .on("end", function (e) {
-      e.target.removePin(true);
-  });
   // Scene 설정
-  var scene_list = '#intro img, #wow img, .committee-list, #sponsor ul'.split(', ');
+  var scene_list = '#wow p, .committee-list, #sponsor ul, #keynote p, #main-poster img, #eve-festival p'.split(', ');
   scene_list.forEach(function (trigger_el, idx) {
+    // console.log(trigger_el);
     var scroll_el = trigger_el;
     var about_scene = new SM.Scene({
       'triggerElement': trigger_el,
       'triggerHook': 0,
       // 'reverse': false,
-      'duration': 500,
-      'offset': -500
+      'duration': 700,
+      'offset': -800
     })
     .setClassToggle(trigger_el, 'fade-in')
     .addIndicators()
     .addTo(ctrl)
-    .on("progress", function (e) {
-      console.log(e.progress);
-      if (0 < e.progress) {
-        if (idx==0) {
-          $('.intro-tab').addClass('about-act');
-        }
-        if (idx==1) {
-          $('.wow-tab').addClass('about-act');
-        }
-        if (idx==2) {
-          $('.committee-tab').addClass('about-act');
-        }
-        if (idx==3) {
-          $('.sponsor-tab').addClass('about-act');
-        }
-      }
-      if (e.progress === 1 || e.progress === 0) {
-        if (idx==0) {
-          $('.intro-tab').removeClass('about-act');
-        }
-        if (idx==1) {
-          $('.wow-tab').removeClass('about-act');
-        }
-        if (idx==2) {
-          $('.committee-tab').removeClass('about-act');
-        }
-        if (idx==3) {
-          $('.sponsor-tab').removeClass('about-act');
-        }
-      }
-    });
+    // .on('progress', function (e) {
+    //   // console.log(e.progress);
+    //   if (0 < e.progress) {
+    //     if (idx==0) {
+    //       $('.intro-tab').addClass('about-act');
+    //     }
+    //     if (idx==1) {
+    //       $('.wow-tab').addClass('about-act');
+    //     }
+    //     if (idx==2) {
+    //       $('.committee-tab').addClass('about-act');
+    //     }
+    //     if (idx==3) {
+    //       $('.sponsor-tab').addClass('about-act');
+    //     }
+    //   }
+    //   if (e.progress === 1 || e.progress === 0) {
+    //     if (idx==0) {
+    //       $('.intro-tab').removeClass('about-act');
+    //     }
+    //     if (idx==1) {
+    //       $('.wow-tab').removeClass('about-act');
+    //     }
+    //     if (idx==2) {
+    //       $('.committee-tab').removeClass('about-act');
+    //     }
+    //     if (idx==3) {
+    //       $('.sponsor-tab').removeClass('about-act');
+    //     }
+    //   }
+    // });
     
   });
 
