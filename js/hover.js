ae96163b7d0cae5335d47451be4227137f9b3ee9
aeree <dofl5576@gmail.com>
2017-11-30 21:08:14 +0900

    최종 올림

---
+++ js/hover.js
@@ -0,0 +1,48 @@
+
+          $(".grid").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/grid_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src', 'imgs/grid_1.gif');
+        });
+
+        $(".firework").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/firework_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src', 'imgs/firework_1.gif');
+        });
+
+        $(".night").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/night_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src','imgs/night_1.gif');
+        });
+
+        $(".diamond").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/diamond_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src', 'imgs/diamond_1.gif');
+        });
+
+        $(".dot").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/dot_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src', 'imgs/dot_1.gif');
+        });
+
+        $(".stripe").hover(
+        function(){ // Mouse Over
+            $(this).find('img').attr('src', 'imgs/gifyellow/stripe_1.gif');
+        },
+        function(){ // Mouse Out
+            $(this).find('img').attr('src', 'imgs/stripe_1.gif');
+        });
